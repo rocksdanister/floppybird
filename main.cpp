bfda4c0941436f99ef88c16c8e2d798842985ded
Dani John <rocksdanister@users.noreply.github.com>
2017-03-27 23:17:32 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -42,42 +42,43 @@ int main(int argc, char** argv)
    glutInit(&argc, argv);
    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
 glutGameModeString("10x10");
-   test:
+   display_test:
    if (glutGameModeGet(GLUT_GAME_MODE_POSSIBLE))
     glutEnterGameMode();
    else {
-   	 printf("Select Screen Resolution, if you are unsure select Windowmode\n"
-   	 		"1. 1920x1080\n2. 1600x900\n3. 1280x720\n4. 1368x768 \n5. 1360x768\n6. 800x600\n7. 640x480\n8. Window(vsync problem)");
+   	 printf("Select Screen Resolution, if you are unsure select Window mode\n"
+   	 		"1. 1920x1080\n2. 1600x900\n3. 1280x720\n4. 1368x768 \n"
+   	 		"5. 1366x768 \n6. 800x600\n7. 640x480\n8. Window(try to avoid this!)");
    	 int ch;
    	 scanf("%d",&ch);
 
    	 if(ch==8)
    	 {
-     printf("\n*ERROR: The selected resolution not available, "
+     printf("\n*WARNING: The selected resolution not available, "
     	"Enter xrandr in terminal.\nChange parameter in glutGameModeString to available resolution in xrandr.\n"
     	"Currently running in windowmode.\nVsync may not perform correctly resulting in poor performance.\n");
      glutInitWindowSize(1280, 720);
-     glutInitWindowPosition(100, 100);
+     //glutInitWindowPosition(100, 100);
      glutCreateWindow("Floppy Bird");
      glutFullScreen();
      }
     else if(ch==1)
     	glutGameModeString("1920x1080");
-      else if(ch==2)
+    else if(ch==2)
     	glutGameModeString("1600x900");
-      else if(ch==3)
+    else if(ch==3)
     	glutGameModeString("1280x720");
-      else if(ch==4)
+    else if(ch==4)
     	glutGameModeString("1368x768");
-      else if(ch==5)
-    	glutGameModeString("1360x768");
-     else if(ch==6)
+    else if(ch==5)
+   	glutGameModeString("1366x768");
+    else if(ch==6)
     	glutGameModeString("800x600");
-     else 
+    else 
     	glutGameModeString("640x480");
 
     if(ch!=8)
-    goto test;
+    	goto display_test;
     	}
    glutIgnoreKeyRepeat(1);
    glutDisplayFunc(draw); 
