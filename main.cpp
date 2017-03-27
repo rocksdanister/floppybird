3523182b584de808ee7b7c11db17fc1e2dda5d38
Dani John <rocksdanister@users.noreply.github.com>
2017-03-27 07:22:43 +0530

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -41,22 +41,44 @@ int main(int argc, char** argv)
 
    glutInit(&argc, argv);
    glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-   //glutInitWindowSize(1280, 720);
-   //glutInitWindowPosition(100, 100);
-   //glutCreateWindow("Floppy Bird");
-   glutGameModeString("1360x768");
+glutGameModeString("10x10");
+   test:
    if (glutGameModeGet(GLUT_GAME_MODE_POSSIBLE))
     glutEnterGameMode();
    else {
-   printf("\n*ERROR: The selected resolution not available, "
+   	 printf("Select Screen Resolution, if you are unsure select Windowmode\n"
+   	 		"1. 1920x1080\n2. 1600x900\n3. 1280x720\n4. 1368x768 \n5. 1360x768\n6. 800x600\n7. 640x480\n8. Window(vsync problem)");
+   	 int ch;
+   	 scanf("%d",&ch);
+
+   	 if(ch==8)
+   	 {
+     printf("\n*ERROR: The selected resolution not available, "
     	"Enter xrandr in terminal.\nChange parameter in glutGameModeString to available resolution in xrandr.\n"
     	"Currently running in windowmode.\nVsync may not perform correctly resulting in poor performance.\n");
-    //... running window mode instead
-    glutInitWindowSize(1280, 720);
-    glutInitWindowPosition(100, 100);
-    glutCreateWindow("Floppy Bird");
-    glutFullScreen();  
-    }
+     glutInitWindowSize(1280, 720);
+     glutInitWindowPosition(100, 100);
+     glutCreateWindow("Floppy Bird");
+     glutFullScreen();
+     }
+    else if(ch==1)
+    	glutGameModeString("1920x1080");
+      else if(ch==2)
+    	glutGameModeString("1600x900");
+      else if(ch==3)
+    	glutGameModeString("1280x720");
+      else if(ch==4)
+    	glutGameModeString("1368x768");
+      else if(ch==5)
+    	glutGameModeString("1360x768");
+     else if(ch==6)
+    	glutGameModeString("800x600");
+     else 
+    	glutGameModeString("640x480");
+
+    if(ch!=8)
+    goto test;
+    	}
    glutIgnoreKeyRepeat(1);
    glutDisplayFunc(draw); 
    glutIdleFunc(mixedStepLoop); 
