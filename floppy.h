e86299015029e2391ae0086fbc7fa2e8ad066b37
Dani John <rocksdanister@users.noreply.github.com>
2017-04-16 10:53:32 +0530

    Update floppy.h

--- floppy.h
+++ floppy.h
@@ -1,3 +1,4 @@
+// Function declarations & global variables.
 extern double queuedMilliseconds,prev0,fps,responseTime;
 extern int resX,resY,movementX,insidebrick,gameover,startFlag;
 extern double movementY;
